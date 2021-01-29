c8d1c449bc701bafa1078be0e24cdce5c1e45efa
Mike Bickerton <mb@bicko.org.uk>
2021-01-29 23:16:44 +0000

    FHS

---
+++ 03-wordpress-7.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ] ; then
+  echo "Please run this as root." 1>&2
+  exit 1
+fi
+
+#Install Ansible, if not already installed (likely it is)
+test ! -f /usr/local/bin/ansible && apt -y install python3-pip && pip3 install ansible
+
+#Install git, if not already installed
+test ! -f /usr/bin/git && apt -y install git
+
+#Get the repo, if not already present
+cd ~
+test ! -d docker-prep && git clone https://github.com/Bicko/docker-prep.git
+
+#Get latest yaml, etc
+cd docker-prep && git pull
+
+ansible-playbook ~/docker-prep/03-wordpress-7.yml
